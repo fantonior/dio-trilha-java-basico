aff8dd0aee3e45c9f90b88c84425f3fe5d7e4bb1
fantonior <fantonior@yahoo.com.br>
2024-09-03 22:53:56 -0300

    metodos

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+  public static void main(String[] args) {
+    int myvar = 4;
+    int mytest = 7;
+    int qpee;
+
+    qpee = myvar == 3 ? 1 : 0;
+    System.out.println(qpee);
+    
+
+
+  }
+}
