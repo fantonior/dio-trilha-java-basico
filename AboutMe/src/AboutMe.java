e29f769733d227ece62074a23f90a13d7910d1dd
fantonior <fantonior@yahoo.com.br>
2024-09-05 00:17:49 -0300

    scanner

--- AboutMe/src/AboutMe.java
+++ AboutMe/src/AboutMe.java
@@ -1,13 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  * AboutMe
  */
 public class AboutMe {
 
   public static void main(String[] args) {
-    String nome = args[0];
+    /*String nome = args[0];
     String sobrenome = args[1];
     int idade = Integer.valueOf(args[2]);
     double altura = Double.valueOf(args[3]);
+    */
+  
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite seu nome");
+    String nome = scanner.next();
+
+    System.out.println("Digite seu sobrenome");
+    String sobrenome = scanner.next();
+    System.out.println("Digite sua idade");
+    int idade = scanner.nextInt();
+
+    System.out.println("Digite sua altura");
+    Double altura = scanner.nextDouble();
 
     System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
     System.out.println("Tenho " + idade + " anos ");
